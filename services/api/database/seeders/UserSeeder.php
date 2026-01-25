eb04ba9a3f4b1b37ea05be71248151ec4fd8efe1
DemonHashira <v.logodazhki@outlook.com>
2026-01-25 23:17:58 +0200

    feat: update user seeder with predefined users and roles

--- services/api/database/seeders/UserSeeder.php
+++ services/api/database/seeders/UserSeeder.php
@@ -2,15 +2,53 @@
 
 namespace Database\Seeders;
 
+use App\Models\Organization;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $org = Organization::query()->where('slug', 'otaku-corner')->firstOrFail();
+
+        $users = [
+            [
+                'name' => 'Viktor Logodazhki',
+                'email' => 'vlogodazhki@otakucorner.test',
+                'role' => 'Administrator',
+            ],
+            [
+                'name' => 'Kiril Hadzhiyski',
+                'email' => 'khadzhiyski@otakucorner.test',
+                'role' => 'Order Manager',
+            ],
+            [
+                'name' => 'Nikolay Pugyov',
+                'email' => 'npugyov@otakucorner.test',
+                'role' => 'Warehouse Manager',
+            ],
+            [
+                'name' => 'Aleksandar Ivanov',
+                'email' => 'aivanov@otakucorner.test',
+                'role' => 'Inventory Controller',
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            $user = User::updateOrCreate(
+                ['email' => $userData['email']],
+                [
+                    'organization_id' => $org->id,
+                    'name' => $userData['name'],
+                    'email' => $userData['email'],
+                    'password' => Hash::make('password'),
+                    'is_active' => true,
+                ],
+            );
+
+            $user->syncRoles([$userData['role']]);
+        }
     }
 }
